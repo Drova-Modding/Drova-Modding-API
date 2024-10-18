1af5fd33e5417f16909e5420e857c783727eba5c
Florian Pabst <f.pabst@gmx.net>
2024-10-18 23:49:33 +0200

    Make it hoverable, add a panel and make it selectable

--- Drova Modding API/Access/OptionMenuAccess.cs
+++ Drova Modding API/Access/OptionMenuAccess.cs
@@ -1,4 +1,8 @@
-﻿using UnityEngine;
+﻿using Il2Cpp;
+using Il2CppDrova.GUI;
+using Il2CppSystem.Collections.Generic;
+using MelonLoader;
+using UnityEngine;
 using UnityEngine.UI;
 
 namespace Drova_Modding_API.Access
@@ -24,24 +28,81 @@ namespace Drova_Modding_API.Access
         }
 
         /**
-         * Add a header to the option menu and set a icon. This is not persistent and will be removed when the option menu is closed.
+         * Add a header and panel to the option menu and set a icon. This is not persistent and will be removed when the option menu is closed.
          * @param icon The icon of the header (not the background).
+         * @param name The name of the header. It needs to be unique and named liked: GUI_Button_OptionTab_YOUROPTION.
          * @return The created header game object.
          */
-        public GameObject AddHeader(Sprite icon)
+        public GameObject AddPanel(Sprite icon, string name)
         {
+            if(!name.StartsWith("GUI_Button_OptionTab_"))
+            {
+                MelonLogger.Error("The name of the header needs to start with GUI_Button_OptionTab_");
+                return null;
+            }
             GameObject root = GetRootOfHeader();
             GameObject newHeader = GameObject.Instantiate(root.transform.GetChild(0).gameObject, root.transform);
             Image image = newHeader.transform.GetChild(0).GetComponent<Image>();
+            newHeader.name = name;
             image.m_OverrideSprite = icon;
             image.sprite = icon;
             image.MarkDirty();
-            return newHeader;
+            var panel = this.AddPanel(newHeader);
+            var componentToRegister = newHeader.GetComponent<GUI_ButtonNavigationAnimationElement>();
             
+            root.GetComponent<GUI_ButtonNavigationAnimation>().AddElement(componentToRegister);
+            GetGUIWindow().GetComponent<GUI_GameMenu_NavigationSwitcher>().legacyElements.Add(componentToRegister);
+            OverrideNavigation(newHeader, panel);
+            return newHeader;
         }
 
         /**
-         * Get the root of the option window.
+         * Add the header and panel to the navigatio system.
+         * @param header The header to add.
+         * @param panel The panel to add.
+         */
+        private void OverrideNavigation(GameObject header, GameObject panel)
+        {
+            var manager = GetGUIWindow().GetComponent<GUI_Window_Options>();
+            var guiPanel = new GUI_Window_ATabManager.GUI_TabPanel
+            {
+                Instance = panel.GetComponent<GUI_GameMenu_APanel>(),
+                Id = header.transform.GetSiblingIndex() + 1
+            };
+            guiPanel.OverrideNavigationElement(header.GetComponent<GUI_ButtonNavigationAnimationElement>());
+            guiPanel.SetButtonName();
+
+            manager.GuiPanels.Add(guiPanel);
+            manager.SetupPanelNavigationElements();
+            // Rerender panel, otherwise its empty.
+            manager.ChangePanel(0);
+        }
+
+        /**
+         * Add a panel to the option menu.
+         * @param header The header to add the panel to. It needs a name with GUI_Button_OptionTab_YOUROPTION
+         * @return The created panel game object.
+         */
+        private GameObject AddPanel(GameObject header)
+        {
+            var root = GetRootOfOptionWindow();
+            var newPanel = GameObject.Instantiate(root.transform.GetChild(4).gameObject, root.transform);
+            newPanel.name = "Panel" + header.name.Substring(header.name.LastIndexOf('_'));
+            newPanel.SetActive(false);
+            return newPanel;
+        }
+
+        /**
+         * Get the GUI window.
+         * @return The root of the option window.
+         */
+        public GameObject GetGUIWindow()
+        {
+            return GameObject.Find("SceneRoot/GUI_Window_Options(Clone)");
+        }
+
+        /**
+         * Get the root of the panel where header and panels are.
          * @return The root of the option window.
          */
         public GameObject GetRootOfOptionWindow()
@@ -49,6 +110,10 @@ namespace Drova_Modding_API.Access
             return GameObject.Find("SceneRoot/GUI_Window_Options(Clone)/Panel");
         }
 
+        /**
+         * Get the root of the header.
+         * @return The root of the header.
+         */
         public GameObject GetRootOfHeader()
         {
             return GameObject.Find("SceneRoot/GUI_Window_Options(Clone)/Panel/Header");
