3cded25afde4e51260328a38d4d1672aefaf9113
Florian Pabst <f.pabst@gmx.net>
2024-10-28 18:43:03 +0100

    Added Addressable paths for Bandits and Creatures

---
+++ Drova Modding API/Access/AddressableAccess.cs
@@ -0,0 +1,157 @@
+﻿using UnityEngine.AddressableAssets;
+
+namespace Drova_Modding_API.Access
+{
+    /// <summary>
+    /// This class is used to access addressable assets in the game.
+    /// </summary>
+    public static class AddressableAccess
+    {
+#pragma warning disable CS1591 // Missing XML comment for publicly visible type or member
+        public static class Creatures
+        {
+            public readonly static AssetReferenceGameObject BabyRedFox = new("51a6ee791f3d52d4089f70f92eefb50c");
+            public readonly static AssetReferenceGameObject BabyRedFox_Outside = new("8d6a68c9156c2c24cb40f477df68676d");
+            public readonly static AssetReferenceGameObject BabySnapper = new("1e92a6820f3d13d408da435608478922");
+            public readonly static AssetReferenceGameObject Banshee = new("33e6ec036bd339342a0f7d473d410783");
+            public readonly static AssetReferenceGameObject Bear = new("2baa51ad3cad0d348afe060d0449d644");
+            public readonly static AssetReferenceGameObject BigSpider = new("b36bdb3cb752e0840bcaf11a585b3840");
+            public readonly static AssetReferenceGameObject BigSpider_AuwaldDungeon = new("329bcb96e94daed439b9331e28fa0c0f");
+            public readonly static AssetReferenceGameObject BigSpider_Leuchtwald_Spot_01 = new("163634c5b75fc8945af15690872a5eec");
+            public readonly static AssetReferenceGameObject BigSpider_Leuchtwald_Spot_02 = new("7aa46c94e7b26b14aa6385e67e5a572b");
+            public readonly static AssetReferenceGameObject Bloodquito_Auwald = new("7212c105223d75441b2563b7df24d717");
+            public readonly static AssetReferenceGameObject Bloodquito_Kamikaze = new("4c65d8b3179d23143a1792a98e2da1c2");
+            public readonly static AssetReferenceGameObject Bloodquito_Rotmoor = new("31351ce9230d16544b6782cec2d50485");
+            public readonly static AssetReferenceGameObject Bloodquito_RotmoorEnemiesWithFrogs = new("2a13a97d67dcf8341b4ae1ab643d0181");
+            public readonly static AssetReferenceGameObject Boar = new("087395a8b20322e4ba29389a3c59bb72");
+            public readonly static AssetReferenceGameObject Boar_Intro = new("e620d1b9f4e53f147be440947240a967");
+            public readonly static AssetReferenceGameObject Boar_Luminous = new("d737d56949aada949b94e77c91b67cbe");
+            public readonly static AssetReferenceGameObject Boar_Luminous_ExpeditionML = new("d70513ace8a5c314fab3dbefa4215521");
+            public readonly static AssetReferenceGameObject Boar_Luminous_ExpeditionRL = new("d4775bd78a9bd9c469c86277f5a49e4e");
+            public readonly static AssetReferenceGameObject BrutusEnd_Outro_Creatures = new("11fe21a6cb2bb9143a2f30b1ea104a9c");
+            public readonly static AssetReferenceGameObject ChaosSpirit = new("57596bc7dde55a643a583b1a5fc35ecc");
+            public readonly static AssetReferenceGameObject DiggerPig = new("2880dce5faad4ec4faffe56e7455fd29");
+            public readonly static AssetReferenceGameObject EliteRipper = new("319a5f954e5769e4189465e6d7c3b686");
+            public readonly static AssetReferenceGameObject EliteRipper_HuntTheRipper = new("4901f09293ef66b4ea2f120dbc8bf1ab");
+            public readonly static AssetReferenceGameObject EnemyFlesh_Baneling = new("e7be5c77fb11fab45b8499146ad77cca");
+            public readonly static AssetReferenceGameObject EnemyFlesh_Melee = new("ec9a94427d10cb343994b40f1a4d07fc");
+            public readonly static AssetReferenceGameObject EnemyFlesh_Ranged = new("13fda0e8c2e4a9947b9862d9eecfa2b7");
+            public readonly static AssetReferenceGameObject EnemyFlesh_Tank = new("efb513c266fa3504b801d0c7e6588d82");
+            public readonly static AssetReferenceGameObject Foglet = new("2d79e4bbe44bd0b40b4f10a75a472f27");
+            public readonly static AssetReferenceGameObject Foglet_Weak = new("df53493f509f3c64686396a5b081afe3");
+            public readonly static AssetReferenceGameObject Foglet_WithoutFog = new("aa1e9756c7039d44998d1bc796c2a944");
+            public readonly static AssetReferenceGameObject Frog_Friendly_Hansi = new("1afa11896d061bf44b31f03d6a7ed0fa");
+            public readonly static AssetReferenceGameObject Frog_Friendly_Player = new("633013391da827349940257d9ca4fdb5");
+            public readonly static AssetReferenceGameObject Frog_Melee = new("d54c45d1460c12b45a5293a173daf80e");
+            public readonly static AssetReferenceGameObject Frog_Ranged = new("a433c7f05e5681249b83044780621d13");
+            public readonly static AssetReferenceGameObject Golem = new("0fef090425b472e41a8ddf36fe87820c");
+            public readonly static AssetReferenceGameObject Golem_ChallengeCave = new("fa831b725ab4e7d4192110c585a533f2");
+            public readonly static AssetReferenceGameObject Golem_Library = new("1bfcbaa6dd41b0c4a8464fcfbfdc8bde");
+            public readonly static AssetReferenceGameObject GreatTusk = new("d5901e300aca7964ab4b4c86994f3d9f");
+            public readonly static AssetReferenceGameObject Boar_Luminous_GreatTuskMinion = new("a6d4a69cad59a1f41b651fa2ce49dbba");
+            public readonly static AssetReferenceGameObject Boar_Luminous_GreatTuskMinion_EndPhase = new("5de3395207768774395330b116ab535c");
+            public readonly static AssetReferenceGameObject Boar_Luminous_GreatTuskMinion_Intimidate = new("cc998235c92235b45847cd58e31a43f4");
+            public readonly static AssetReferenceGameObject LightBear_GreatTuskMinion = new("d386b164d17214549bcb5b932148a148");
+            public readonly static AssetReferenceGameObject ShadowRipper_GreatTuskMinion = new("ba4f8883823ef2340b2999b65d826360");
+            public readonly static AssetReferenceGameObject Harpy = new("bfb1082c84fbee04d96b472fed196f97");
+            public readonly static AssetReferenceGameObject HungryRipper = new("fae77915e46118b4daefed2e46af6241");
+            public readonly static AssetReferenceGameObject HungryRipper_InstantAggro = new("12b628f9d4e840e4ea7ae14b0e8b6c8e");
+            public readonly static AssetReferenceGameObject Leshen = new("bd482a9137360984b816de867f6c8031");
+            public readonly static AssetReferenceGameObject LightBear = new("3429924057db8b04da759a8f9b5647cc");
+            public readonly static AssetReferenceGameObject LightBear_ExpeditionML = new("65be2ab0fc819ae4782ee1a03868cf99");
+            public readonly static AssetReferenceGameObject Menshee = new("84b8e02baaf13fa49a8e09286da17bc2");
+            public readonly static AssetReferenceGameObject MortarShroom = new("2288cef00272c55479cba06eb5595663");
+            public readonly static AssetReferenceGameObject RedFox = new("93ffb2e9c36d4db479b12ddff2b65f8f");
+            public readonly static AssetReferenceGameObject Ripper = new("8074729ab27467d42bbb9c783c2f9758");
+            public readonly static AssetReferenceGameObject Ripper_InstantAggro = new("7a46684d61e95c94ca3621cda90a5e37");
+            public readonly static AssetReferenceGameObject ShadowRipper = new("b80e30fffb7cf9244a9a113fd10a9044");
+            public readonly static AssetReferenceGameObject ShadowRipper_Ambush = new("ea9242bb13f7fed40ba9b9f01ef2004e");
+            public readonly static AssetReferenceGameObject ShadowRipper_Ambush_LittleAggroRange = new("49948d8ded5b22b438b0a3cae800a759");
+            public readonly static AssetReferenceGameObject ShadowRipper_ExpeditionRunFollower_02 = new("4979e0a0a671d5c4eb413ce5a993eb09");
+            public readonly static AssetReferenceGameObject ShadowRipper_ExpeditionRunLeader = new("ff1bd949022a551469343be16b43c1bf");
+            public readonly static AssetReferenceGameObject Snapper = new("505832a81f5f1b24c85fa1e924bb3401");
+            public readonly static AssetReferenceGameObject Snapper_ExtraWeak = new("d9a420e90c41073459b576f18f79ce38");
+            public readonly static AssetReferenceGameObject Snapper_ForestDungeonBoss = new("1f6656c16fcd8f049991431ce16d19e2");
+            public readonly static AssetReferenceGameObject Snapper_Weak = new("7602e26598efeab4e90e5c30d1b4f203");
+            public readonly static AssetReferenceGameObject Spitter = new("11ddec44c7acd6648969edb40d18f8ff");
+            public readonly static AssetReferenceGameObject Spitter_ExtraWeak = new("07bc2039b459f4749a247bad65debcff");
+            public readonly static AssetReferenceGameObject Spitter_ForestDungeonBoss = new("d6d65fe9c1c6b964582ab850ac15b519");
+            public readonly static AssetReferenceGameObject Spitter_Weak = new("331fdfbe16a78f4448f023847f0db83d");
+            public readonly static AssetReferenceGameObject Sprigga = new("bbe3be5d348fcee46b3613518a7688d7");
+            public readonly static AssetReferenceGameObject Sprigga_Young_Hidden = new("3c7727536bf1971419197a99d245cf35");
+            public readonly static AssetReferenceGameObject Sprigga_Young_Normal = new("e89dab4908ae7e443af0227517b6c9c6");
+            public readonly static AssetReferenceGameObject Stalker_Red = new("9f427388ff60c70408f4b7482dc02b3a");
+            public readonly static AssetReferenceGameObject StoneFist = new("c1cae7ce9d214c74285b0ea75ebdcb50");
+            public readonly static AssetReferenceGameObject StoneFist_BigGroupAggro = new("4fc0f4795d704104db5b29b425510dc5");
+            public readonly static AssetReferenceGameObject StoneFist_DeepMine = new("b844f52eb5a6ed149bb99d3c87148fa7");
+            public readonly static AssetReferenceGameObject WaterAmbusher = new("9682f12f0f92ace4c8ad8d374eaecb29");
+            public readonly static AssetReferenceGameObject WaterDevil = new("7fe8275adbf01f44e9cda96cbefd10e7");
+            public readonly static AssetReferenceGameObject WaterDevil_Red = new("0dd84e246420d8d4c9b6f4dc51c255e7");
+            public readonly static AssetReferenceGameObject WaterRat = new("18d0be35a54cdce448b34771277ebbcc");
+            public readonly static AssetReferenceGameObject WaterRat_Bone = new("36baa4cfce7294747adb7b6122d72ba6");
+            public readonly static AssetReferenceGameObject WaterRat_Young = new("4a2d978e4259290478d41849cb1a390e");
+            public readonly static AssetReferenceGameObject Worm = new("c379a9248937d734f989f8bbe969bb2b");
+            public readonly static AssetReferenceGameObject Critter_Cat_Aggressive_01 = new("20cc5bc122608b842994166636ef47be");
+            public readonly static AssetReferenceGameObject Critter_Cat_Aggressive_02 = new("f717c934b01f90942991d5c6b053e79a");
+            public readonly static AssetReferenceGameObject Critter_Cat_Lovely_01 = new("6cf439d79f37fcd4886b96cab8ea14f1");
+            public readonly static AssetReferenceGameObject Critter_Cat_Lovely_02 = new("29f2980b354fa1d4aa4e855e1a311455");
+            public readonly static AssetReferenceGameObject Critter_Cat_RunAway_01 = new("83743e6fa5a112043a2eecc40cf52c49");
+            public readonly static AssetReferenceGameObject Critter_Cat_RunAway_02 = new("9bd709e227facf04dac1b4434d8f5584");
+            public readonly static AssetReferenceGameObject Critter_Mikesch = new("4426b0b80b01cda4c91cf5af08460833");
+            public readonly static AssetReferenceGameObject Critter_Dog_Type1_01 = new("5bd628719fe27b04bbebf179003b4152");
+            public readonly static AssetReferenceGameObject Critter_Dog_Type1_02 = new("d7306bb090af6d74d927dc568574229c");
+            public readonly static AssetReferenceGameObject Critter_Dog_Type2_01 = new("c587ba0f052ac0d4fb4f33ee441719f3");
+            public readonly static AssetReferenceGameObject Critter_Dog_Type2_02 = new("0f1a580c97c098c4593aa217fa62d6a8");
+            public readonly static AssetReferenceGameObject Critter_Dog_Type3_01 = new("8ac6ebdccb3fcc8459272591c9e50ca8");
+            public readonly static AssetReferenceGameObject Critter_Dog_Type3_02 = new("6a0e11447f91ded49a22405468c4ee83");
+            public readonly static AssetReferenceGameObject Critter_BlackGrouse_Female = new("39e37f92b63d11e4ca94dffd6506adbc");
+            public readonly static AssetReferenceGameObject Critter_BlackGrouse_Male = new("22e51275b43769245bc92a89a1ab9d6b");
+            public readonly static AssetReferenceGameObject Critter_NurturingSprite = new("a2f627e936382244d8f338036666fb0d");
+            public readonly static AssetReferenceGameObject Critter_Sheep_NonDialog = new("8daa97a5efff9814da4d8f8ec7b2e1ac");
+            public readonly static AssetReferenceGameObject Critter_Sheep_01 = new("265872ec03b135e4f834394447c19c4c");
+            public readonly static AssetReferenceGameObject Critter_Sheep_02 = new("755649392c3968c4e9966d1ec83b10af");
+            public readonly static AssetReferenceGameObject Critter_Sheep_03 = new("25de0407bbbb8a64c861f2f627644722");
+            public readonly static AssetReferenceGameObject Critter_Sheep_04 = new("e79ab4fa51f674a42923f7e3e6eee3b8");
+            public readonly static AssetReferenceGameObject Critter_Sheep_Generic_01 = new("e76925d2c48ca354ea24bbc2ffec1eea");
+            public readonly static AssetReferenceGameObject Brutus = new("459bdb01f0871ef46ab14c64437c6da1");
+            public readonly static AssetReferenceGameObject Draugr_Axe = new("5a0eff0ec14f41e4c84e0413700e57f6");
+            public readonly static AssetReferenceGameObject Draugr_Mage = new("5a2a3332e8b140e4bb309dc4c5e1fef4");
+            public readonly static AssetReferenceGameObject Draugr_Mage_BrutusCave = new("2a1685f5beec5d54887e34b37f9652b0");
+            public readonly static AssetReferenceGameObject Draugr_Mage_RedTower_Stage_03_Letter = new("38ebc3e19cb12c741bc25e8cf8b2f3b8");
+            public readonly static AssetReferenceGameObject Draugr_Spear = new("ae33ae04d956679488283c4e5c4d0fd6");
+            public readonly static AssetReferenceGameObject Draugr_Spear_BrutusCave = new("3faed44e3ee9d8e4e85012c10d89d206");
+            public readonly static AssetReferenceGameObject Draugr_Spear_BrutusCave_StartAlive = new("533fe7decf91d4745b593c87a70d7482");
+            public readonly static AssetReferenceGameObject Draugr_Spear_RedTower_Letter = new("54e0b5f96687416478151bd1b79af488");
+            public readonly static AssetReferenceGameObject Draugr_Spear_RedTower_Stage_03 = new("5ab55190d508e274a9decc006c0b8555");
+            public readonly static AssetReferenceGameObject Draugr_Sword_Library_Letter = new("362eebe03c217c44980007b103cb52ad");
+            public readonly static AssetReferenceGameObject Draugr_Sword_Shield = new("875174c4ae3af0b4f9a8909d7e2cf19e");
+            public readonly static AssetReferenceGameObject Draugr_Sword_Shield_BrutusCave = new("a745dc0727c50bd4a92d9371f43f8c31");
+            public readonly static AssetReferenceGameObject Draugr_Sword_Shield_BrutusCave_StartAlive = new("38200b82c9de6394ca2de8e8587850f7");
+            public readonly static AssetReferenceGameObject Draugr_Sword_Shield_DraugrDungeonIntro = new("33a13dc2b9e9d2b45b8de539c7eb5afa");
+            public readonly static AssetReferenceGameObject Draugr_Sword_Shield_RedTower_Stage_03 = new("134ed053837b1b54b818cd5101674a6d");
+            public readonly static AssetReferenceGameObject Critter_Rat = new("d1f30492047fe37428db886184506d79");
+            public readonly static AssetReferenceGameObject Critter_Snake_Adder = new("3e24d9f9bca9f714b91cfc62062e55a8");
+        }
+
+        public static class Bandits
+        {
+            public readonly static AssetReferenceGameObject Human_Bandit_Expedition_01 = new("12b962da18197054a8bdd3d3b644b87d");
+            public readonly static AssetReferenceGameObject Human_Bandit_Expedition_02 = new("36f40e4342329e446995bcf7a8fb6529");
+            public readonly static AssetReferenceGameObject Human_Bandit_Expedition_03 = new("75f0bdac5c2bde649a5c4206103aa81d");
+            public readonly static AssetReferenceGameObject Human_Bandit_Expedition_04 = new("e8fce139081c0a44482a24c648a6bc93");
+            public readonly static AssetReferenceGameObject Human_Bandit_Mine_01 = new("d474e6a73fd504e42bc425c1b2f02d94");
+            public readonly static AssetReferenceGameObject Human_Bandit_Mine_02 = new("1cfa8a615a5d2d147af25e62cd8476c6");
+            public readonly static AssetReferenceGameObject Human_Bandit_Mine_03 = new("42dec63f2dbd94d45bbccb1576a943e3");
+            public readonly static AssetReferenceGameObject Human_Bandit_RuinKasern_01 = new("93edeac08052f36438b3cb4b1d1c87b9");
+            public readonly static AssetReferenceGameObject Human_Bandit_RuinKasern_02 = new("2ad0a57419c939244b4e054da3434569");
+            public readonly static AssetReferenceGameObject Human_Bandit_RuinKasern_03 = new("5dd87072476744549bb29fd5492be6c5");
+            public readonly static AssetReferenceGameObject Human_Bandit_Ruins_01 = new("d76c9f4620013354a97567c0deee9576");
+            public readonly static AssetReferenceGameObject Human_Bandit_Ruins_02 = new("84b0ca74577fcf5469776b8737987416");
+            public readonly static AssetReferenceGameObject Human_Bandit_Ruins_03 = new("92fc5f256340888489fcf9df7f13a655");
+            public readonly static AssetReferenceGameObject Human_Bandit_Ruins_04 = new("a0432afea28fa9c44ab8fe428de2dda7");
+            public readonly static AssetReferenceGameObject Human_Bandit_Ruins_05 = new("09e245f7a274a8c418e4ff977e05941e");
+            public readonly static AssetReferenceGameObject Human_Bandit_Snaga = new("5d2aee37bd73ec94e8338f54b4fb1314");
+        }
+#pragma warning restore CS1591 // Missing XML comment for publicly visible type or member
+    }
+}
